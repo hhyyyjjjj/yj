38628eb8bc82d0b08fb12f5e3a0a8d494ad87958
hhyyyjjjj <ujin508916@gmail.com>
2025-05-03 11:37:02 +0900

    Add files via upload

---
+++ a.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+import streamlit as st
+
+st.write("Hello, *World!* :sunglasses:")
+
+st.write('파이썬의 세계')
+'# 파이썬'
+'## *너무* 어렵네'
+'### *어려워* 어려워'
+
+import streamlit as st
+
+st.markdown("### 이 기호는 | 입니다.")
+
+
+import streamlit as st
+
+# ASCII 아트를 변수에 저장
+unicorn = """
+  |^-----^|
+  V  o o  V
+   |  Y  |
+    \ Q /
+    / - |
+   |    |
+   |    |  
+   || (___|====
+"""
+
+# Streamlit 앱으로 출력
+st.title("dog!")
+st.text(unicorn)
